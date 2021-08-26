0df85f36aff72976b6eb40363a0a5dba6f5f169f
theriaultjulien <theriault.julien98@gmail.com>
2021-08-26 16:42:39 -0400

    lab#1_FINAL

--- Conversions/Conversions/frfmConversions.cs
+++ Conversions/Conversions/frfmConversions.cs
@@ -17,133 +17,15 @@ namespace Conversions
             InitializeComponent();
         }
 
-        private void btnBinaireDecimal_Click(object sender, EventArgs e)
-        {
-            //Lire les données 
-            string sBinaire = txtBinaireADec.Text;
-            int nbDecimal = 0;
-            int i = 0;
-            int exposant = 0;
+        //Méthodes publiques pour éviter la répétition de code pour les validations
 
-            //Valider les données
+        public bool isBinaire(string sBinaire)
+        {
+            int i = 0;
             if (sBinaire.Length > 8)
             {
                 MessageBox.Show("Le nombre binaire doit être sur 8 bits ou moins");
-                return;
-            }
-            while ((i < sBinaire.Length) && (sBinaire[i]=='0' || sBinaire[i] == '1'))
-            {
-                i++;
-            }
-            if (i < sBinaire.Length)
-            {
-                MessageBox.Show("Un nombre binaire peut seulement être composé de 1 ou de 0");
-                return;
-            }
-
-            //Traitement
-            for (i = sBinaire.Length - 1; i >= 0; i--)
-            {
-                if (sBinaire[i] == '1')
-                    nbDecimal += (int)Math.Pow(2, exposant);
-                exposant ++;
-            }
-
-            // Afficher résultat
-            txtDecimalDeBin.Text = nbDecimal.ToString();
-        }
-
-        private void btnDecBin_Click(object sender, EventArgs e)
-        {
-            string sDecimal = txtDecimalABin.Text;
-            string sBinaire = "";
-            int nbDecimal;
-            bool valide;
-
-            //Valider données
-            valide = int.TryParse(sDecimal, out nbDecimal);
-            if (!valide)
-            {
-                MessageBox.Show("Le nombre entré est invalide");
-                return;
-            }
-
-            //Traitement
-                //Fonction pour reverse la string qui va sortir
-            string Reverse(string s)
-            {
-                char[] charArray = s.ToCharArray();
-                Array.Reverse(charArray);
-                return new string(charArray);
-            }
-
-            while (nbDecimal != 0)
-            {
-                sBinaire += (nbDecimal % 2).ToString();
-                nbDecimal /= 2;
-            }
-
-            //Afficher résultat
-            txtBinaireDeDec.Text = Reverse(sBinaire);
-        }
-
-        private void btnHexaDec_Click(object sender, EventArgs e)
-        {
-            string sHexa = txtHexaADec.Text;
-            int nbDecimal;
-            bool valide;
-
-            //Valider données
-            valide = sHexa.All("0123456789abcdefABCDEF".Contains);
-            if (!valide)
-            {
-                MessageBox.Show("Le nombre entré est invalide");
-                return;
-            }
-
-            //Traitement
-            nbDecimal = Convert.ToInt32(sHexa, 16);
-
-
-            //Afficher
-            txtDecimalDeHexa.Text = nbDecimal.ToString();
-
-        }
-
-        private void btnDecHexa_Click(object sender, EventArgs e)
-        {
-            string sDecimal = txtDecimalAHexa.Text;
-            int nbDecimal;
-            string sHexa = "";
-            bool valide;
-
-            //Valider
-            valide = int.TryParse(sDecimal, out nbDecimal);
-            if (!valide)
-            {
-                MessageBox.Show("Le nombre entré est invalide");
-                return;
-            }
-
-            //Traitement (Le overload de la fonction ToString "X" met un int en string Hexa directement)
-            sHexa = nbDecimal.ToString("X");
-
-            //Afficher
-            txtHexaDeDec.Text = sHexa;
-        }
-
-        private void btnBinHexa_Click(object sender, EventArgs e)
-        {
-            //Lire les données 
-            string sBinaire = txtBinAHexa.Text;
-            string sHexa;
-            int i = 0;
-
-            //Valider les données
-            if (sBinaire.Length > 8)
-            {
-                MessageBox.Show("Le nombre binaire doit être sur 8 bits ou moins");
-                return;
+                return false;
             }
             while ((i < sBinaire.Length) && (sBinaire[i] == '0' || sBinaire[i] == '1'))
             {
@@ -152,36 +34,118 @@ namespace Conversions
             if (i < sBinaire.Length)
             {
                 MessageBox.Show("Un nombre binaire peut seulement être composé de 1 ou de 0");
-                return;
+                return false;
+            }
+            return true;
+        }
+
+        public bool isDecimal(string sDecimal, out int nbDecimal)
+        {
+            nbDecimal = 0;
+            if (!(int.TryParse(sDecimal, out int iDecimal)))
+            {
+                MessageBox.Show("Le nombre entré est invalide");
+                return false;
             }
 
-            //Traitement
-                //convert binary to dec using Convert.ToInt32 then convert the dec to hexa using .ToString("X")
-            sHexa = Convert.ToInt32(sBinaire, 2).ToString("X");
+            if (iDecimal < 0 || iDecimal > 255)
+            {
+                MessageBox.Show("Le nombre entré doit être entre 0 et 255");
+                return false;
+            }
+            nbDecimal = iDecimal;
+            return true;
+        }
+
+        public bool isHexa(string sHexa)
+        {
+            if (sHexa.ToUpper().All(CBase.HexValues.Contains) && sHexa.Length <= 2) return true;
+            else MessageBox.Show("L'entrée Hexadécimale est invalide ou elle contient plus de 2 caractères");
+            return false;
+        }
+
+
+        private void btnBinaireDecimal_Click(object sender, EventArgs e)
+        {
+            //Lire les données 
+            string sBinaire = txtBinaireADec.Text;
+            bool valide;
+
+            //Valider les données
+            valide = isBinaire(sBinaire);
+            if (!valide) return;
+
+            // Afficher résultat
+            txtDecimalDeBin.Text = CBase.BinaireToDecimal(sBinaire);
+        }
+
+        private void btnDecBin_Click(object sender, EventArgs e)
+        {
+            string sDecimal = txtDecimalABin.Text;
+            bool valide;
+
+            //Valider données
+            valide = isDecimal(sDecimal, out int nbDecimal);
+            if (!valide) return;
+
+            //Afficher résultat
+            txtBinaireDeDec.Text = CBase.DecimalToBinaire(nbDecimal);
+        }
+
+        private void btnHexaDec_Click(object sender, EventArgs e)
+        {
+            string sHexa = txtHexaADec.Text;
+            bool valide;
+
+            //Valider données
+            valide = isHexa(sHexa);
+            if (!valide) return;
 
             //Afficher
-            textBox10.Text = sHexa;
+            txtDecimalDeHexa.Text = CBase.HexaToDecimal(sHexa);
+
+        }
+
+        private void btnDecHexa_Click(object sender, EventArgs e)
+        {
+            string sDecimal = txtDecimalAHexa.Text;
+            bool valide;
+
+            //Valider
+            valide = isDecimal(sDecimal, out int nbDecimal);
+            if (!valide) return;
+
+            //Afficher
+            txtHexaDeDec.Text = CBase.DecimalToHexa(nbDecimal);
+        }
+
+        private void btnBinHexa_Click(object sender, EventArgs e)
+        {
+            //Lire les données 
+            string sBinaire = txtBinAHexa.Text;
+            bool valide;
+
+            //Valider les données
+            valide = isBinaire(sBinaire);
+            if (!valide) return;
+
+            //convert binary to dec then convert the dec to hexa
+            //Afficher
+            textBox10.Text = CBase.DecimalToHexa(Convert.ToInt32(CBase.BinaireToDecimal(sBinaire)));
         }
 
         private void btnHexaBin_Click(object sender, EventArgs e)
         {
             string sHexa = txtHexaABin.Text;
-            string sBinaire;
             bool valide;
 
             //Valider données
-            valide = sHexa.All("0123456789abcdefABCDEF".Contains);
-            if (!valide)
-            {
-                MessageBox.Show("Le nombre entré est invalide");
-                return;
-            }
+            valide = isHexa(sHexa);
+            if (!valide) return;
 
-            //Traitement
-            sBinaire = Convert.ToString(Convert.ToInt32(sHexa, 16), 2);
-
+            // Convertir le Hex en Dec et ensuite le Dec en binaire avec les fonctions déjà créées
             //Afficher
-            txtBinDeHexa.Text = sBinaire;
+            txtBinDeHexa.Text = CBase.DecimalToBinaire(Convert.ToInt32(CBase.HexaToDecimal(sHexa)));
 
         }
     }
