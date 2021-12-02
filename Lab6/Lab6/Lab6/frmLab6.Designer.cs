8966c9ab605d6f47a952106a86c63df454d8fc62
Julien Theriault <theriault.julien98@gmail.com>
2021-12-02 16:41:18 -0500

    Remise Lab6

---
+++ Lab6/Lab6/Lab6/frmLab6.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Lab6
+{
+    partial class frmLab6
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_lab6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_lab6
+            // 
+            this.btn_lab6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_lab6.Location = new System.Drawing.Point(130, 89);
+            this.btn_lab6.Name = "btn_lab6";
+            this.btn_lab6.Size = new System.Drawing.Size(253, 122);
+            this.btn_lab6.TabIndex = 0;
+            this.btn_lab6.Text = "Lab6 exercice données en mémoire";
+            this.btn_lab6.UseVisualStyleBackColor = true;
+            this.btn_lab6.Click += new System.EventHandler(this.btn_lab6_Click);
+            // 
+            // frmLab6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 301);
+            this.Controls.Add(this.btn_lab6);
+            this.Name = "frmLab6";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_lab6;
+    }
+}
+
---
+++ Lab6/Lab6/Lab6/frmLab6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class frmLab6 : Form
+    {
+        public frmLab6()
+        {
+            InitializeComponent();
+        }
+        int Fonction1(char pCar, string pCaracs, ref byte lo_ol)
+        {
+            string chaine = "Comment ça va ?";
+            pCaracs = "allo";
+            byte b;
+            byte[] tOctets = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            b = 77;
+            pCar = (char)(chaine[7] + tOctets[2]); // vérifier dans une table ASCII
+            lo_ol = b;
+
+            return tOctets[1];
+        }
+
+
+        private void btn_lab6_Click(object sender, EventArgs e)
+        {
+            byte octet;
+            char lettre;
+            int resultat;
+
+            string chaine;
+            byte[] tOctets = { 123, 124, 125, 126, 127, 128, 129, 130 };
+            char[] tCarac = { 'L', 'u', 't', 'i', 'n' };
+
+            octet = 32;
+            lettre = 'Z';
+            resultat = -1;
+
+            tOctets[4] = 1;
+            chaine = "Joyeux Noël";
+            lettre = chaine[3];
+            tCarac = chaine.ToCharArray();
+            tCarac[0] = 'j';
+
+            resultat = Fonction1(lettre, chaine, ref tOctets[4]);
+
+        }
+    }
+}
